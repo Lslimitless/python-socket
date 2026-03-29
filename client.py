01271fb649949411bbd9568225c9f789b160c7e8
YongDeok <81160857+Lslimitless@users.noreply.github.com>
2026-03-29 14:48:00 +0000

    add nickname, /w

--- client.py
+++ client.py
@@ -12,26 +12,27 @@ while True:
     except:
         continue
 
-while True:
-    try:
-        nickname = int(input("이름:"))
-        break
-    except:
-        continue
+try:
+    nickname = str(input("이름:"))
+    print(nickname)
+except:
+    nickname = "Guest"+str(random.randint(0,65535))
 
 def receive():
     while True:
         try:
             data = clientSocket.recv(1024).decode('utf-8')
             if data:
-                print(f"SERVER:{data}")
+                print(data)
 
         except:
             break
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((HOST, PORT))
+clientSocket.sendall(nickname.encode('utf-8'))
 print("서버와 연결됨.")
+
 
 threading.Thread(target=receive, daemon=True).start()
 
--- server.py
+++ server.py
@@ -6,25 +6,40 @@ PORT = 0
 
 clients = []
 
-def handle_client(conn, addr):
-    print(f"{addr}에서 연결")
-    clients.append(conn)
+def handle_client(conn, addr, nick):
+    print(f"{addr}({nick})에서 연결")
+    clients.append({'conn':conn, 'nick':nick})
+    
     try:
         while True:
-            data = conn.recv(1024)
+            data = conn.recv(1024).decode('utf-8')
             if data:
-                msg = data.decode('utf-8')
-                print(f"{addr}:{msg}")
+                if data[0] == "/":
+                    dataSplit = data.split(maxsplit=2)
+                    if len(dataSplit) >= 3:
+                        if dataSplit[0].lower() == "/w":
+                            print(f"{nick} -> {dataSplit[1]}:{dataSplit[2:][0]}")
+                            for client in clients:
+                                if client['nick'] == data.split()[1]:
+                                    client['conn'].sendall(f"{nick} -> me:{dataSplit[2:][0]}".encode('utf-8'))
+    
+                else:
+                    msg = data
 
-                for client in clients:
-                    if client != conn:
-  
-                        client.sendall(f"{addr}:{msg}".encode('utf-8'))
+                    for client in clients:
+                        if client['conn'] == conn:
+                            print(f"{nick}:{msg}")
+
+                        else:
+                            client['conn'].sendall(f"{nick}:{msg}".encode('utf-8'))
     except:
         pass
     finally:
         print(f"{addr}의 연결이 종료됨.")
-        clients.remove(conn)
+        for client in clients:
+            if client['conn'] == conn:
+                clients.remove(client)
+
         conn.close()
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -36,7 +51,8 @@ print(f"{assigned_port} 포트에서 서버 시작.")
 
 while True:
     conn, addr = serverSocket.accept()
+    nick = conn.recv(1024).decode('utf-8')
 
-    threading.Thread(target=handle_client, args=(conn, addr)).start()
+    threading.Thread(target=handle_client, args=(conn, addr, nick)).start()
     
 serverSocket.close()
