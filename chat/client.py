9b93f56fd2bda11ab390901f39372d8c41d50672
YongDeok <81160857+Lslimitless@users.noreply.github.com>
2026-04-15 13:57:01 +0000

    code_fix

--- chat/client.py
+++ chat/client.py
@@ -1,39 +1,149 @@
+from dataclasses import dataclass, asdict
+import json
 import socket
 import random
 import threading
 import customtkinter as ctk
+import PIL
+from io import BytesIO
+import requests
 
-def login(event):
-    HOST = ipEntry.get()
-    PORT = int(portEntry.get())
-    nickname = nickEntry.get()
+@dataclass
+class Message:
+    type : str
+    nick : str
+    msg : str
+    to : str
 
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((HOST, PORT))
-    clientSocket.sendall(nickname.encode('utf-8'))
-    print("서버와 연결됨.")
+    def encoding(self) -> bytes:
+        return json.dumps(asdict(self)).encode()
+    
+    @classmethod
+    def decoding(cls, raw_data: bytes):
+        data = json.loads(raw_data.decode())
+        return cls(**data)
+    
+def focus_next(event):
+    event.widget.tk_focusNext().focus()
+
+def connection(*event):
+    try:
+        HOST = ipEntry.get()
+        PORT = int(portEntry.get())
+        global nickname
+        nickname = nickEntry.get() if nickEntry.get() else "Guest"+str(random.randint(0,9999))
+
+        global clientSocket
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.connect((HOST, PORT))
+
+        isConnect = True
+        loginUI.place_forget()
+        chatUI.place(relwidth=1, relheight=1)
+        clientSocket.sendall(nickname.encode())
+        print("서버와 연결됨.")
+
+    except:
+        print("입력값을 다시 확인하세요.")
+
+def quit(*event):
+    clientSocket.sendall("QUIT".encode())
+    isConnect = False
+    chatUI.place_forget()
+    loginUI.place(relx=0.5, rely=0.5, anchor="center")
+    clientSocket.close()
+
+def chatHistoryAppend(msg):
+    chatHistory.configure(state="normal")
+    chatHistory.insert("end", msg+"\n")
+    chatHistory.see("end")
+    chatHistory.configure(state="disabled")
+
+def chat(*event):
+    if inputEntry.get():
+        data = inputEntry.get()
+        inputEntry.delete("0", "end")
+
+        dataSplit = data.split(maxsplit=2)
+        if data[0] == "/":
+            if dataSplit[0].lower() == "/w" and len(dataSplit) >= 3:
+                to = dataSplit[1]
+                msg = dataSplit[2]
+
+        else:
+            to = "all"
+            msg = data
+            
+        send_data = json.dumps({'type':"chat", 'nick':nickname, 'msg':msg, 'to':to})
+        # print(data)
+        # if to == "all":
+        #     chatHistoryAppend("{} : {}".format(nickname, msg))
+        # else:
+        #     chatHistoryAppend("me -> {} : {}".format(nickname, msg))
+
+        clientSocket.sendall(send_data.encode())
+
+isConnect = False
+nickname = ""
 
 root = ctk.CTk()
 root.geometry("270x480")
 root.title("Socket Chat")
-ctk.set_appearance_mode("dark")  # "System" (standard), "Dark", "Light"
+ctk.set_appearance_mode("System")  # "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme("blue")
 
-ipFortFrame = ctk.CTkFrame(root)
-ipFortFrame.pack(pady=(10,0))
+# 로그인 UI
+loginUI = ctk.CTkFrame(root, fg_color="transparent")
+loginUI.place(relx=0.5, rely=0.5, anchor="center")
+
+response = requests.get("https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/python-programming-language-icon.png")
+pythonImage = ctk.CTkImage(light_image=PIL.Image.open(BytesIO(response.content)),
+                         dark_image=PIL.Image.open(BytesIO(response.content)),
+                         size=(100,100))
+logoLabel = ctk.CTkLabel(loginUI, image=pythonImage, text="")
+logoLabel.pack(pady=(10,0))
+
+ipFortFrame = ctk.CTkFrame(loginUI, fg_color="transparent")
+ipFortFrame.pack(pady=(20,0))
 
 ipEntry = ctk.CTkEntry(ipFortFrame, placeholder_text="IP", width=150, height=35, corner_radius=10)
 ipEntry.pack(side='left')
+ipEntry.bind("<Return>", command=focus_next)
 
 portEntry = ctk.CTkEntry(ipFortFrame, placeholder_text="Port", width=100, height=35, corner_radius=10)
 portEntry.pack(side='right')
+portEntry.bind("<Return>", command=focus_next)
 
-nickEntry = ctk.CTkEntry(root, placeholder_text="Nickname", width=250, height=35, corner_radius=10)
-nickEntry.pack(pady=(2, 0))
-nickEntry.bind("<Return>", command=login)
+nickEntry = ctk.CTkEntry(loginUI, placeholder_text="Nickname", width=250, height=35, corner_radius=10)
+nickEntry.pack(pady=(2,0))
+nickEntry.bind("<Return>", command=connection)
 
-loginButton = ctk.CTkButton(root, text="Sign in", command=login, width=100, height=35, corner_radius=10)
-loginButton.pack(pady=(2, 0))
+loginButton = ctk.CTkButton(loginUI, text="Connect", command=connection, width=100, height=35, corner_radius=10)
+loginButton.pack(pady=10)
+
+# 채팅창 UI
+chatUI = ctk.CTkFrame(root, fg_color="transparent")
+
+topMenu = ctk.CTkFrame(chatUI, fg_color="skyblue", height=35, corner_radius=0)
+topMenu.pack(side="top", fill="x")
+
+quitButton = ctk.CTkButton(topMenu, text="Quit", command=quit, width=35, height=35, corner_radius=10)
+quitButton.pack(side="right")
+
+inputUI = ctk.CTkFrame(chatUI, fg_color="transparent")
+inputUI.pack(side="bottom", fill="x")
+
+inputButton = ctk.CTkButton(inputUI, text="Enter", command=chat, width=70, height=35, corner_radius=10)
+inputButton.pack(side="right")
+
+inputEntry = ctk.CTkEntry(inputUI, placeholder_text="", height=35, corner_radius=10)
+inputEntry.pack(side="left", fill="x", expand=True)
+inputEntry.bind("<Return>", command=chat)
+
+chatHistory = ctk.CTkTextbox(chatUI)
+chatHistory.configure(state="disabled")
+chatHistory.pack(fill="both", expand="True")
+
 
 # HOST = '0.0.0.0'
 
@@ -53,12 +163,27 @@ loginButton.pack(pady=(2, 0))
 def receive():
     while True:
         try:
-            data = clientSocket.recv(1024).decode('utf-8')
-            if data:
+            recv_data = clientSocket.recv(1024)
+            if recv_data:
+                data = json.loads(recv_data.decode())
                 print(data)
+                if data['type'] == "chat":
+                    if data['to'] == nickname:
+                        msg = "{} -> me : {}".format(data['nick'], data['msg'])
+                    
+                    elif data['nick'] == nickname and data['to'] != "all":
+                        msg = "me -> {} : {}".format(data['to'], data['msg'])
+
+                    else:
+                        msg = "{} : {}".format(data['nick'], data['msg'])
+                    
+                    chatHistoryAppend(msg)
+
+                elif data['type'] == "sys":
+                    chatHistoryAppend(data['msg'])
 
         except:
-            break
+            continue
 
 threading.Thread(target=receive, daemon=True).start()
 
@@ -67,6 +192,5 @@ root.mainloop()
 # while True:
 #     msg = input()
 #     clientSocket.sendall(msg.encode('utf-8'))
-
 
 clientSocket.close()
--- chat/server.py
+++ chat/server.py
@@ -1,52 +1,92 @@
+from dataclasses import dataclass, asdict
+import json
 import socket
 import threading
 
 HOST = 'localhost'
 PORT = 0
 
-clients = []
+clients = {}
 
-def handle_client(conn, addr, nick):
+@dataclass
+class Message:
+    type : str
+    nick : str
+    msg : str
+    to : str
+
+    def encoding(self) -> bytes:
+        return json.dumps(asdict(self)).encode()
+    
+    @classmethod
+    def decoding(cls, raw_data: bytes):
+        data = json.loads(raw_data.decode())
+        return cls(**data)
+
+def handle_client(conn, addr):
+    # clients.append({'conn':conn, 'nick':nick})
+    clients[conn] = conn.recv(1024).decode()
+    nick = clients[conn]
+    conn.sendall(json.dumps({'type':"sys", 'msg':"서버에 접속하였습니다."}).encode())
     print(f"{addr}({nick})에서 연결")
-    clients.append({'conn':conn, 'nick':nick})
-    print(len(clients))
+    print(f"연결된 클라이언트 수 : {len(clients)}")
     
     try:
-        while True:
-            data = conn.recv(1024).decode('utf-8')
-            if data:
-                # /명령어 처리
-                if data[0] == "/":
-                    # /w 귓속말 기능
-                    dataSplit = data.split(maxsplit=2)
-                    if dataSplit[0].lower() == "/w" and len(dataSplit) >= 3:
-                        print(f"{nick} -> {dataSplit[1]}:{dataSplit[2:][0]}")
-                        # 자신에게 귓속말 하는지 여부
-                        if nick != data.split()[1]:
-                            for client in clients:
-                                if client['nick'] == data.split()[1]:
-                                    client['conn'].sendall(f"{nick} -> me:{dataSplit[2:][0]}".encode('utf-8'))
-                        else:
-                            conn.sendall("자신에게는 귓속말 할 수 없습니다.".encode('utf-8'))
+        while clients[conn]:
+            recv_data = conn.recv(1024)
+            
+            if recv_data.decode() == "QUIT" or recv_data == b'':
+                print(f"{addr}({clients.get(conn)})의 연결이 종료됨.")
+                del clients[conn]
+                print(f"연결된 클라이언트 수 : {len(clients)}")
+                conn.close()
+                break
+        
+            elif recv_data:
+                data = json.loads(recv_data.decode())
+                print(data)
 
-                else:
-                    msg = data
+                if data['type'] == "chat":
+                    # 존재하지 않는 닉네임에게 귓속말 할 시
+                    if data['to'] != "all" and (not data['to'] in clients.values()):
+                        try:
+                            conn.sendall(json.dumps({'type':"sys", 'msg':"대상을 찾을 수 없습니다."}).encode())
+                            continue
+                        except:
+                            pass
+                        
+                    # 자신에게 귓속말 할 시
+                    elif data['to'] != "all" and data['to'] == data['nick']:
+                        try:
+                            conn.sendall(json.dumps({'type':"sys", 'msg':"자신에게 귓속말 할 수 없습니다."}).encode())
+                            continue
+                        except:
+                            pass
+                    
 
-                    for client in clients:
-                        if client['conn'] == conn:
-                            print(f"{nick}:{msg}")
+                    else:
+                        # send_data = Message(type="chat", nick=data["nick"], msg=data["msg"], to="")
+                        for target_conn, target_nick in clients.items():
+                            if data['to'] == target_nick or \
+                                data['to'] != "all" and data['nick'] == target_nick or \
+                                data['to'] == "all":
+                                try:
+                                    target_conn.sendall(recv_data)
+                                except:
+                                    pass
 
-                        else:
-                            client['conn'].sendall(f"{nick}:{msg}".encode('utf-8'))
     except:
-        pass
-    finally:
-        print(f"{addr}의 연결이 종료됨.")
-        for client in clients:
-            if client['conn'] == conn:
-                clients.remove(client)
-
+        print(f"{addr}({clients.get(conn)})의 연결이 비정상적으로 종료됨.")
         conn.close()
+        del clients[conn]
+        print(f"연결된 클라이언트 수 : {len(clients)}")
+
+    finally:
+        try:
+            conn.close()
+            del clients[conn]
+        except:
+            pass
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((HOST, PORT))
@@ -57,8 +97,7 @@ print(f"IP:{ip}, {assigned_port} 포트에서 서버 시작.")
 
 while True:
     conn, addr = serverSocket.accept()
-    nick = conn.recv(1024).decode('utf-8')
-
-    threading.Thread(target=handle_client, args=(conn, addr, nick)).start()
+    threading.Thread(target=handle_client, args=(conn, addr)).start()
     
 serverSocket.close()
+print("서버가 종료됨.")
